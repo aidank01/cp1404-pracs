5f3ae090c6ce217c391de7cd1268b641da7bc5a3
aidank01 <aidan.kayrooz@my.jcu.edu.au>
2016-09-17 02:05:43 +1000

    fill out class attributes

--- prac_07/languages.py
+++ prac_07/languages.py
@@ -1,5 +1,49 @@
 """mooo"""
 
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+from prac_07.programming_language import ProgrammingLanguage
+
+def main():
+
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    ruby.name()
+    ruby.is_dynamic()
+    ruby.is_reflection()
+    ruby.year()
+    print("mooo", ruby.name)
+
+
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    python.name
+    python.is_dynamic
+    python.is_reflection
+    python.year()
+    print()
+
+
+    vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    vb.name
+    vb.is_dynamic
+    vb.is_reflection
+    vb.year()
+    print()
+
+
+
+    # bus = Car(180, 'Bus')
+    # bus.drive(30)
+    # print("fuel =", bus.fuel)
+    # print("odo =", bus.odometer)
+    # print(bus)
+    #
+    # print("Car {}, {}".format(bus.fuel, bus.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=bus))
+    #
+    # limo = Car(100, 'Limo')
+    # limo.add_fuel(20)
+    # print("fuel =", limo.fuel)
+    # limo.drive(115)
+    # print("odo =", limo.odometer)
+    #
+    # print(limo)
+
+    main()
--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,4 +1,8 @@
 
 
 class ProgrammingLanguage:
-    def __init__(self):
+    def __init__(self, name="", is_dynamic= (), is_reflection = (), year = ()):
+        self.name = name
+        self.is_dynamic()
+        self.is_reflection()
+        self.year = input(int)
